9b849e1007e77d17360050b267adf2ec88e2eb1d
Martin V. <72466872+maatov@users.noreply.github.com>
2025-12-03 21:17:30 +0100

    Add files via upload

--- day3/sol.py
+++ day3/sol.py
@@ -5,20 +5,6 @@ def load(fname):
 
 #inp = load('test-input.txt')
 inp = load('input.txt')
-print(inp)
-
-def find2digsNum(line):
-    line = [int(x) for x in list(line)]
-    first = max(line[:-1])
-    first_i = line[:-1].index(first)
-    second = max(line[first_i+1:])
-    return first*10 + second
-
-res = 0
-for line in inp:
-    res += find2digsNum(line)
-
-print(f'solution1 {res}')
 
 def findNdigsNum(line,n):
     line = [int(x) for x in list(line)]
@@ -32,9 +18,13 @@ def findNdigsNum(line,n):
         num += digit
     return num
 
+res = 0
+for line in inp:
+    res += findNdigsNum(line,2)
+print(f'solution1 {res}')
+
 res2 = 0
 for line in inp:
-    res2 += findNdigsNum(line,12)
-    
+    res2 += findNdigsNum(line,12)    
 print(f'solution2 {res2}')
     
